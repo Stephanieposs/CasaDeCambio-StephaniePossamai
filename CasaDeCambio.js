8f5385d57833a66ff4ccec6626f02f1fb34aa324
stephanie possamai <stephaniepossamaiamanda@gmail.com>
2023-11-14 13:19:38 -0300

    conversao da moeda de entrada para moeda de saida

--- CasaDeCambio.js
+++ CasaDeCambio.js
@@ -1,14 +1,20 @@
+buscarMoedasAJAX(carregarSelectMoedas1);
+buscarMoedasAJAX(carregarSelectMoedas2);
 
 
-buscarMoedasAJAX(carregarSelectMoedas);
-cotacao();
-dataAtual();
+var botao = document.getElementById("idButton")
 
-//AJAX
+botao.addEventListener("click", buscarCotacaoEntrada)
+var dataAt = dataAtual()
+var conversao1 = 0;
+var conversao2 = 0;
+
+// pegar cotacao de uma moeda 
+// fazer conversao da moeda pra rela e de real pra outra moeda
+
+
 function buscarMoedasAJAX(carregarSelectMoedas) {
-    //AJAX - Fazer uma requisição HTTPs / Consumo de API
     var xhr = new XMLHttpRequest()
-
     xhr.open("GET", "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/Moedas?$top=100&$format=json&$select=simbolo,nomeFormatado,tipoMoeda")
 
     xhr.addEventListener("load", function () {
@@ -19,36 +25,69 @@ function buscarMoedasAJAX(carregarSelectMoedas) {
     })
     xhr.send()
 }
-
-function carregarSelectMoedas(moedas) {
-    //carregando as moedas no select da tela... 
+function carregarSelectMoedas1(moedas) {
+    //carregando as options com as moedas
     let listaMoedas = document.getElementById("idMoedas")
-    let listaMoedas2 = document.getElementById("idMoedas2")
-
     for (let i = 0; i < moedas.value.length; i++) {
-        console.log(moedas.value[i].simbolo)
-
         let optionMoeda = document.createElement("option")
         optionMoeda.value = moedas.value[i].simbolo
         optionMoeda.innerText = moedas.value[i].nomeFormatado
 
         listaMoedas.appendChild(optionMoeda)
+    }
+}
+function carregarSelectMoedas2(moedas) {
+    //carregando as options com as moedas
+    let listaMoedas2 = document.getElementById("idMoedas2")
+    for (let i = 0; i < moedas.value.length; i++) {
+        let optionMoeda = document.createElement("option")
+        optionMoeda.value = moedas.value[i].simbolo
+        optionMoeda.innerText = moedas.value[i].nomeFormatado
+
         listaMoedas2.appendChild(optionMoeda)
     }
-
 }
 
-function cotacao() {
+// converte a moeda de entrada para real
+function buscarCotacaoEntrada() {
+    var moedaEntrada = document.getElementById("idMoedas").value
+    var valor = document.getElementById("idValor").value
     var xhr = new XMLHttpRequest()
 
-    xhr.open("GET", "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@moeda='EUR'&@dataInicial='11-09-2023'&@dataFinalCotacao='01-31-2025'&$top=100&$format=json&$select=paridadeCompra,paridadeVenda,cotacaoCompra,cotacaoVenda,dataHoraCotacao,tipoBoletim")
-
+    console.log(moedaEntrada)
+    xhr.open("GET", "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?@moeda='"+moedaEntrada+"'&@dataCotacao='"+dataAt+"'&$top=100&$format=json&$select=paridadeCompra,paridadeVenda,cotacaoCompra,cotacaoVenda,dataHoraCotacao,tipoBoletim")
+    
     xhr.addEventListener("load", function () {
         let resposta = xhr.responseText
         let moedas = JSON.parse(resposta)
         console.log("moedas")
         console.log(moedas)
-        console.log(moedas.value[0])
+
+        let tamanho = moedas.value.length 
+        // conversao da primera moeda para reais
+        conversao1 = valor * moedas.value[tamanho-1].cotacaoCompra;
+        console.log(conversao1)
+    })
+    xhr.send()
+    buscarCotacaoSaida()
+}
+// converte real para a moeda de saida
+function buscarCotacaoSaida() {
+    var moedaSaida = document.getElementById("idMoedas2").value
+    var xhr = new XMLHttpRequest()
+
+    xhr.open("GET", "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?@moeda='"+moedaSaida+"'&@dataCotacao='"+dataAt+"'&$top=100&$format=json&$select=paridadeCompra,paridadeVenda,cotacaoCompra,cotacaoVenda,dataHoraCotacao,tipoBoletim")
+    
+    xhr.addEventListener("load", function () {
+        let resposta = xhr.responseText
+        let moedas = JSON.parse(resposta)
+        console.log("moedas")
+        console.log(moedas)
+
+        let tamanho = moedas.value.length 
+        // conversao de reais pra moeda de saida
+        conversao2 = conversao1 / (moedas.value[tamanho-1].cotacaoVenda);
+        console.log(conversao2)
     })
     xhr.send()
 }
@@ -57,9 +96,8 @@ function cotacao() {
 function dataAtual() {
     const date = new Date();
     const ano = date.getFullYear();
-    console.log(ano);
     const dia = date.getDate();
     const mes = date.getMonth() + 1;
-    var data = (ano+"-"+mes+"-"+dia)
+    var data = (mes+"-"+dia+"-"+ano)
     return data
 }
